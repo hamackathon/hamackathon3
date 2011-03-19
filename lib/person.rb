9072cd971d9fbb150edbf8beb82c16c6d63d2f45
Masakuni Kato <mackato@gmail.com>
2011-03-19 14:45:33 +0900

    Personのシリアライズとファイルへの保存・削除処理を追加しました。

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,13 @@
 # conding: utf-8
 require 'yaml'
+require 'erb'
+require 'digest/md5'
+require 'fileutils'
 
 class Person
   MALE = "m"
   FEMALE = "f"
+  
   attr_accessor :name, :address, :tel, :birthday, :gender, :logs
   
   def initialize(params={})
@@ -18,4 +22,48 @@ class Person
     params = YAML.load(File.read(file_path))
     Person.new(params)
   end
+  
+  def self.data_dir=(path)
+    @@data_dir = path
+  end
+  
+  def self.data_dir
+    @@data_dir
+  end
+  
+  def to_yaml
+    person = self
+    erb = ERB.new(TEMPLATE)
+    erb.result(binding)
+  end
+  
+  def id
+    @id ||= Digest::MD5.hexdigest("#{self.name}+#{self.tel}")
+  end
+  
+  def path
+    @path ||= File.join(Person.data_dir, self.id[0, 2], self.id[2, 2], "#{self.id}.yaml")
+  end
+  
+  def save()
+    dir = File.expand_path('..', self.path)
+    FileUtils.mkdir_p(dir)
+    File.open(self.path, 'w') do |f|
+      f.puts self.to_yaml
+    end
+    return true
+  end
+  
+  def delete()
+    File.delete(self.path)
+  end
+  
+  TEMPLATE =<<EOS
+name: "<%= person.name %>"
+address: "<%= person.address %>"
+tel: "<%= person.tel %>"
+birthday: "<%= person.birthday %>"
+gender: "<%= person.gender %>"
+logs:
+EOS
 end
--- tests/person_test.rb
+++ tests/person_test.rb
@@ -1,3 +1,4 @@
+# conding: utf-8
 require File.expand_path('test_helper', File.dirname(__FILE__))
 require 'person'
 
@@ -9,6 +10,10 @@ class PersonTest < Test::Unit::TestCase
     'birthday' => "1970-01-01",
     'gender' => Person::MALE
   }
+  
+  def setup
+    Person.data_dir = File.join(File.dirname(__FILE__), 'out')
+  end
   
   def test_initialize
     person = Person.new(TARO)
@@ -25,5 +30,44 @@ class PersonTest < Test::Unit::TestCase
     person = Person.load(taro_file)
     assert_not_nil(person)
     assert_equal("山田太郎", person.name)
+  end
+  
+  def test_data_dir
+    assert_not_nil(Person.data_dir)
+  end
+  
+  def test_to_yaml
+    taro = Person.new(TARO)
+    yaml = taro.to_yaml
+    taro_copy = YAML.load(yaml)
+    assert_equal("山田太郎", taro_copy['name'])
+  end
+  
+  def test_id
+    taro_id = Person.new(TARO).id
+    assert_not_nil(taro_id)
+    another_taro_id = Person.new(TARO).id
+    assert_equal(taro_id, another_taro_id)
+  end
+  
+  def test_path
+    taro = Person.new(TARO)
+    assert_not_nil(taro.path)
+  end
+  
+  def test_save
+    taro = Person.new(TARO)
+    assert !File.exist?(taro.path)
+    assert(taro.save())
+    assert File.exist?(taro.path)
+    File.delete(taro.path)
+  end
+  
+  def test_delete
+    taro = Person.new(TARO)
+    taro.save()
+    assert File.exist?(taro.path)
+    taro.delete()
+    assert !File.exist?(taro.path)
   end
 end
